bfb99949668cfdb12a5c0afe41d67dac408e44e2
Shashikant86 <shashikant.jagtap@aol.co.uk>
2015-05-15 00:03:26 +0100

    Removed extra code

--- lib/driller.rb
+++ lib/driller.rb
@@ -4,42 +4,5 @@ require "driller/crawler"
 require "anemone"
 
 module Driller
-  URL = ARGV[0]
-  Anemone.crawl(URL) do |anemone|
-
-anemone.depth_limit = 2
-
-anemone.focus_crawl do |page|
-  page.links.select { |url| url.starts_with? URL }
-end
-
-file = File.new('valid_pages.html', 'w')
-file = File.new('broken.html', 'w')
-file = File.new('slow_pages.html', 'w')
-
-anemone.on_every_page do |page|
-
-  if page.code = 200 && page.code = 301
-    file = File.open('valid_pages.html', 'a')
-    file.puts page.url
-  end
-
-  if page.code != 200 && page.code != 301
-    puts "=======broken======\n"
-    puts page.url
-    file = File.open('broken.html', 'a')
-    file.puts page.url
-  end
-
-
-  if page.response_time > 5000
-    puts "=======Slow Page======\n"
-    puts page.url
-    file = File.open('slow_pages.html', 'a')
-    file.puts page.url
-  end
-
-end
-
-end
+  
 end
--- lib/driller/version.rb
+++ lib/driller/version.rb
@@ -1,3 +1,3 @@
 module Driller
-  VERSION = "0.0.8"
+  VERSION = "0.0.9"
 end
